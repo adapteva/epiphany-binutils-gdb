f125d10538f3c212af57690755e48101fa62cbe7
Ola Jeppsson <ola@adapteva.com>
2015-10-19 20:30:20 +0200

    sim/testsuite:epiphany: Add tests for denormal underflow exception
    sim/testsuite/sim/epiphany/
    	* fmul-denormal-exception.s: New test.
    	* fmul64-denormal-exception.s: New test.
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

---
+++ sim/testsuite/sim/epiphany/fmul64-denormal-exception.s
@@ -0,0 +1,43 @@
+# epiphany testcase for fmul $frd6,$frn6,$frm6 -*- Asm -*-
+# mach: all
+
+	.include "testutils.inc"
+
+	start
+
+sync_ivt:
+	b.l	rti
+swi_ivt:
+	b.l	swi_irq
+
+	.global fmul64
+
+rti:
+	gie
+	mov	r4,%low(fmul64)
+	movt	r4,%high(fmul64)
+	movts	iret, r4
+	rti
+	.balignw 8,0x01a2
+
+fmul64:
+	mova	r6,0xe
+	movts	config,r6
+	mode	1
+	;; lowest possible non-denormal number
+	mov	r20,1
+	mova	r21,0x00100000
+	fmul64	r20,r20,r20
+	fail
+
+swi_irq:
+	movfs	r1,status
+	mova	r2,0x0000f000
+	and	r1,r1,r2
+	lsr	r1,r1,12
+	;; check that only busbit is set
+	sub	r1,r1,8
+	bne	1f
+	pass
+1:
+	fail
